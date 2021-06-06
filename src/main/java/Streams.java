c34b4e5cf97061648a17c24c719c25899c5d463c
konrad <ktalaga90@gmail.com>
2021-06-06 10:19:52 +0100

    map, sorted, limit, average, max, min, and peak methods tested

--- src/main/java/Streams.java
+++ src/main/java/Streams.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Streams {
@@ -15,11 +17,58 @@ public class Streams {
 //
 //        System.out.println(output);
 
-        int total = Stream.of(1, 2, 3, 4, 5, 6, 7)
-                .reduce(0,(sum, num)-> sum += num);
+//        int total = Stream.of(1, 2, 3, 4, 5, 6, 7)
+//                .reduce(0,(sum, num)-> sum += num);
+//
+//        System.out.println(total);
 
-        System.out.println(total);
+//        Stream.of(1, 2, 3, 4, 5, 6, 7)
+//                .map((num)-> num *2)
+//                .forEach(System.out::println);
 
+//        Stream.of(1,2,3,4,5,6,7)
+//                .filter((num) -> num %2 == 0)
+//                .forEach(System.out::println);
+
+//        Stream.of(99, 3, 61, 20, 83, 12, 2, 18, 10)
+//                .sorted()
+//                .forEach(System.out::println);
+//        Stream.of(99, 3, 61, 20, 83, 12, 2, 18, 10)
+//                .sorted()
+//                .limit(4)
+//                .forEach(System.out::println);
+
+//        OptionalDouble average = Stream.of(99, 3, 61, 20, 83, 12, 2, 18, 10).mapToInt((n) -> n).average();
+//
+//        System.out.println(average);
+//        // average terminates
+
+//        int result = Stream.of(99, 3, 61, 20, 83, 12, 2, 18, 10)
+//                .mapToInt((n) -> n)
+//                .max()
+//                .getAsInt();
+//
+//        System.out.println(result);
+
+//        int result = Stream.of(99, 3, 61, 20, 83, 12, 2, 18, 10)
+//                .mapToInt((n) -> n)
+//                .min()
+//                .getAsInt();
+//        System.out.println(result);
+
+//        int out = IntStream.of(99, 3, 61, 20, 83, 12, 2, 18, 10)
+//                .peek(n -> System.out.printf("original: %d%n", n))
+//                .map(n -> n * 2)
+//                .peek(n -> System.out.printf("doubled : %d%n", n))
+//                .filter(n -> n % 3 == 0)
+//                .peek(n -> System.out.printf("filtered: %d%n", n))
+//                .sum();
+//
+//        System.out.println(out);
+
+        IntStream.range(1, 5)
+                .mapToObj(num -> "number: " + num)
+                .forEach(System.out::println);
 
     }
 }
