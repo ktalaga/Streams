970ee737dd7827222a197a172bec3c68a8d277e1
konrad <ktalaga90@gmail.com>
2021-06-06 09:43:33 +0100

    collect termination method tested

--- src/main/java/Streams.java
+++ src/main/java/Streams.java
@@ -1,11 +1,20 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Streams {
 
     public static void main(String[] args){
         List<String> myList = Arrays.asList("amanda", "alex", "sandy", "alina", "sky");
+//
+//        myList.stream().map(String::toUpperCase).forEach(result -> System.out.println(result));
+        List<String> output = Stream.of("amanda", "alex", "sandy", "alina", "sky")
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
 
-        myList.stream().map(String::toUpperCase).forEach(result -> System.out.println(result));
+        System.out.println(output);
+
+
     }
 }
