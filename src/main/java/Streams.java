9ce6bbcba8380fc0ca25aac9a97cfb8c634e50d1
konrad <ktalaga90@gmail.com>
2021-06-06 09:58:09 +0100

    reduce terminal operation tested

--- src/main/java/Streams.java
+++ src/main/java/Streams.java
@@ -9,11 +9,16 @@ public class Streams {
         List<String> myList = Arrays.asList("amanda", "alex", "sandy", "alina", "sky");
 //
 //        myList.stream().map(String::toUpperCase).forEach(result -> System.out.println(result));
-        List<String> output = Stream.of("amanda", "alex", "sandy", "alina", "sky")
-                .map(String::toUpperCase)
-                .collect(Collectors.toList());
+//        List<String> output = Stream.of("amanda", "alex", "sandy", "alina", "sky")
+//                .map(String::toUpperCase)
+//                .collect(Collectors.toList());
+//
+//        System.out.println(output);
 
-        System.out.println(output);
+        int total = Stream.of(1, 2, 3, 4, 5, 6, 7)
+                .reduce(0,(sum, num)-> sum += num);
+
+        System.out.println(total);
 
 
     }
